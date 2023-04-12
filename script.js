41b3fc47d22c02b3a3ff69c21bb7f542e2813e7a
SS4nd <72603119+SS4nd@users.noreply.github.com>
2023-04-12 10:37:56 +0200

    javascript: roterende grid added
    done

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 "use strict";
+
+window.addEventListener("load", initApp);
+
+function initApp() {
+    const grid = document.getElementById('barchart');
+    var numbers = [];
+    for (var x = 0; x < 40; x++) {
+        const bar = document.createElement('div');
+        bar.className = "bar";
+        numbers.push(Math.floor(Math.random() * 300));
+        bar.style.height = numbers[numbers.length - 1].toString() + "px";
+        grid.appendChild(bar);
+    }
+    change(numbers);
+
+}
+
+function change(numbers) {
+    var newNum = numbers;
+    const bars = document.querySelectorAll(".bar");
+
+    setInterval(function() {
+        bars.forEach((b, i) => {
+            try {
+                b.style.height = newNum[i].toString() + "px";
+                b.style.backgroundColor = `rgb(${newNum[i]/2}, ${newNum[i]/2}, ${newNum[i]/2})`;
+            } catch {
+                b.style.height = newNum[39].toString() + "px";
+                b.style.backgroundColor = `rgb(${newNum[39]/2}, ${newNum[39]/2}, ${newNum[39]/2})`;
+            }
+        });
+        newNum.push(newNum.shift());
+    }, 100);
+}
